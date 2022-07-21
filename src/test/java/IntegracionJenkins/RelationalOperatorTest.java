1a853d8665d8091eb1b69a1ef2cc9705fa30fcdf
Alfreed0 <lurochav@espol.edu.ec>
2022-07-20 23:41:52 -0500

    isLess and goodWidth Tests

--- src/test/java/IntegracionJenkins/RelationalOperatorTest.java
+++ src/test/java/IntegracionJenkins/RelationalOperatorTest.java
@@ -2,16 +2,33 @@ package IntegracionJenkins;
 
 import static org.junit.Assert.*;
 
+import java.beans.Transient;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.Test;
 
 public class RelationalOperatorTest {
+
+	private final PrintStream standardOut = System.out;
+	private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+	@BeforeEach
+	public void setUp() {
+		System.setOut(new PrintStream(outputStreamCaptor));
+	}
+
+	@AfterEach
+	public void tearDown() {
+		System.setOut(standardOut);
+	}
 
 	@Test
 	public void isGreaterTest1() {
 		RelationalOperator tester = new RelationalOperator();
 		assertFalse(tester.isGreater(2, 3));
 	}
-	
+
 	@Test
 	public void isGreaterTest2() {
 		RelationalOperator tester = new RelationalOperator();
@@ -23,10 +40,37 @@ public class RelationalOperatorTest {
 		RelationalOperator tester = new RelationalOperator();
 		assertTrue(tester.isLess(4, 4));
 	}
-	
+
 	@Test
 	public void isLessTest2() {
 		RelationalOperator tester = new RelationalOperator();
 		assertFalse(tester.isLess(5, 1));
+	}
+
+	@Test
+	public void isLessTest3() {
+		RelationalOperator tester = new RelationalOperator();
+		assertFalse(tester.isLess(4, 4));
+	}
+
+	@Test
+	public void goodWidthTest1() {
+		RelationalOperator tester = new RelationalOperator();
+		tester.goodWidth(4, 2, 7);
+		assertEquals("The width is correct", outputStreamCaptor.toString().trim());
+	}
+
+	@Test
+	public void goodWidthTest2() {
+		RelationalOperator tester = new RelationalOperator();
+		tester.goodWidth(4, 2, 3);
+		assertEquals("The width is INCORRECT", outputStreamCaptor.toString().trim());
+	}
+
+	@Test
+	public void goodWidthTest3() {
+		RelationalOperator tester = new RelationalOperator();
+		tester.goodWidth(4, 5, 7);
+		assertEquals("The width is INCORRECT", outputStreamCaptor.toString().trim());
 	}
 }
